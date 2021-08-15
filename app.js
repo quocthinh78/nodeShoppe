19cc08015d6d6a6c5588283f4df9d44a7daf5e77
quocthinh78 <thinhvqps11484@fpt.edu.vn>
2021-08-15 09:13:10 +0700

    dsa

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const orderRouter = require("./routes/order");
 var app = express();
 var mongoose = require("mongoose");
 const db = "mongodb+srv://monggodb-shopee:GfQafsGbrISNYpuQ@mongodbshopee.w72gm.mongodb.net/react-shopee?retryWrites=true&w=majority"
-mongoose.connect(db, {
+mongoose.connect("mongodb://localhost:27017/shoppe", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 });
